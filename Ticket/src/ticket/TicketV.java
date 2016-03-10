960f52ca15f7a77fa8a143285dff619dbc90ad68
Asif Mohaimen <saumik01@gmail.com>
2016-03-10 21:18:58 +0600

    Complete Project

--- Ticket/src/ticket/TicketV.java
+++ Ticket/src/ticket/TicketV.java
@@ -6,10 +6,6 @@ import net.sourceforge.barbecue.Barcode;
 import net.sourceforge.barbecue.BarcodeFactory;
 import net.sourceforge.barbecue.BarcodeImageHandler;
 
-import java.awt.Color;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Scanner;
 import org.apache.pdfbox.Overlay;
 import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -21,11 +17,11 @@ import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.xobject.PDPixelMap;
-import java.util.Date;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
@@ -56,6 +52,61 @@ public class TicketV {
     
     private static void processTicket(String name, String Batch, String txn)
     {
-        return;
+        String fileName = "Ticket.pdf";
+        String watermark = "watermark.pdf";
+        File file = new File("testsize.png");
+        File file2 = new File("Ticket.pdf");
+        try{
+            file.delete();
+            file2.delete();
+        }
+        catch(Exception e){}
+        try {
+            PDDocument doc = new PDDocument();
+            PDPage page = new PDPage(PDPage.PAGE_SIZE_A4);
+            PDXObjectImage image = new PDJpeg(doc, new FileInputStream("testsize.jpg"));
+            doc.addPage(page);
+            PDRectangle rect = page.getMediaBox();
+            PDFont fontBold = PDType1Font.HELVETICA_BOLD;
+            PDPageContentStream content = new PDPageContentStream(doc, page);
+            
+            content.beginText();
+            content.setFont(fontBold, 10);
+            content.moveTextPositionByAmount(140, 595);
+            content.drawString(name);
+            content.endText();
+            
+            content.beginText();
+            content.setFont(fontBold, 10);
+            content.moveTextPositionByAmount(140, 580);
+            content.drawString(Batch);
+            content.endText();
+            
+            content.beginText();
+            content.setFont(fontBold, 10);
+            content.moveTextPositionByAmount(180, 565);
+            content.drawString(txn);
+            content.endText();
+            
+            content.drawImage(image, 400, 648);
+            
+            content.close();
+            
+            PDDocument watermarkDoc = PDDocument.load(watermark);
+            
+            Overlay overlay = new Overlay();
+            overlay.overlay(doc,watermarkDoc);
+            watermarkDoc.save(fileName);
+            
+            doc.close();
+            watermarkDoc.close();
+        }
+        catch(Exception e){}
+        
+        File file3 = new File("testsize.jpg");
+        try{
+            file3.delete();
+        }
+        catch(Exception e){}
     }
 }
